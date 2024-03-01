5a473c953e1d96e417dea255f82b003b3eb73505
Shubham1204pandey <125333556+Shubham1204pandey@users.noreply.github.com>
2024-03-01 11:09:07 +0530

    Update start_container.sh

--- scripts/start_container.sh
+++ scripts/start_container.sh
@@ -2,7 +2,7 @@
 set -e
 
 # Pull the Docker image from Docker Hub
-docker pull shubham1204pandey/code-build
+docker pull shubham1204pandey/flask-automation
 
 # Run the Docker image as a container
-docker run -d -p 5000:5000 shubham1204pandey/code-build
+docker run -d -p 5000:5000 shubham1204pandey/flask-automation
